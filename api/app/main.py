9dd941b3218a07275e3d397fbef26c8662d27346
Kaarel Kivisalu <kaarel.kivisalu@gmail.com>
2026-01-20 21:30:50 +0100

    api: add school students and mentors endpoints

--- api/app/main.py
+++ api/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from app.routes.subcontest import router as subcontest_router
 from app.routes.people import router as people_router
+from app.routes.schools import router as schools_router
 
 app = FastAPI(title="EOA API")
 
 app.include_router(subcontest_router)
 app.include_router(people_router)
+app.include_router(schools_router)
 
 
 @app.get("/health")
